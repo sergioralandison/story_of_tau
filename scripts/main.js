74c8ebcb25f9fe944e1ed8e03605d960cc77feee
sergioralandison <sergioralandison@outlook.com>
2021-07-05 17:27:57 +0200

    Total show time function added

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,18 @@
 let numberOfSeasons = 6;
 let numberOfEpisodes = 12;
 
+let episodeTime = 45;
+let commercialTime = 5;
 // ==========================================
+
+// Calculate totalShowTime here
+// ==========================================
+let totalShowTime = (episodeTime + commercialTime) * numberOfEpisodes * numberOfSeasons;
+// ==========================================
+
+
 let paragraph = document.querySelector('#info');
 paragraph.innerText = `${numberOfSeasons} seasons, 
-${numberOfEpisodes} episodes per season`;
+${numberOfEpisodes} episodes per season
+
+Total viewing time: ${totalShowTime} minutes`;
