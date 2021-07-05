b7c503cd885e160fbdc26f26948236639bbae865
sergioralandison <sergioralandison@outlook.com>
2021-07-05 15:53:43 +0200

    Script to display number of seasons and episodes

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Create your variables here
+// ==========================================
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+
+// ==========================================
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, 
+${numberOfEpisodes} episodes per season`;
